87cfac8eda6211ae6a928345b65a2ee8bb25f055
qzmp <lukasz.golebiowski@mail.ru>
2015-05-12 20:55:23 +0200

    Completed piece constructors

--- Chess/Piece.cpp
+++ Chess/Piece.cpp
@@ -23,26 +23,32 @@ double Piece::getValue()
 
 King::King(Color color) : Piece(color)
 {
+	this->value = 20;
 }
 
 Queen::Queen(Color color) : Piece(color)
 {
+	this->value = 8.8;
 }
 
 Rook::Rook(Color color) : Piece(color)
 {
+	this->value = 5.1;
 }
 
 Bishop::Bishop(Color color) : Piece(color)
 {
+	this->value = 3.33;
 }
 
 Knight::Knight(Color color) : Piece(color)
 {
+	this->value = 3.2;
 }
 
 Pawn::Pawn(Color color) : Piece(color)
 {
+	this->value = 1;
 }
 
 list<GameStatus> King::generateMoves(GameStatus& currentStatus, int x, int y)
